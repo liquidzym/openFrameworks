05e72baa631aaf0e2c08a79f3ae12f4e4d99d74d
DomAmato <dominic.a.amato@gmail.com>
2015-09-08 22:36:01 -0500

    VideoWriter working update

--- libs/openFrameworks/video/ofVideoWriter.cpp
+++ libs/openFrameworks/video/ofVideoWriter.cpp
@@ -22,7 +22,7 @@ extern "C" {
 #include "ofUtils.h"
 #include "ofVideoWriter.h"
 
-void ofVideoWriter::setup(const char* filename, int width, int height) {
+void ofVideoWriter::setup(const char* filename, int width, int height, int bitrate, int framerate, AVPixelFormat inputFormat, AVPixelFormat outputFormat) {
 	ofLogNotice("ofVideoWriter") << "Video encoding: " << filename;
 	/* register all the formats and codecs */
 	av_register_all();
@@ -42,6 +42,7 @@ void ofVideoWriter::setup(const char* filename, int width, int height) {
 	/* Add the audio and video streams using the default format codecs
 	 * and initialize the codecs. */
 	video_st = NULL;
+	avcodec_alloc_context3(codec);
 	if (fmt->video_codec != AV_CODEC_ID_NONE) {
 		/* find the video encoder */
 		AVCodecID avcid = fmt->video_codec;
@@ -51,10 +52,26 @@ void ofVideoWriter::setup(const char* filename, int width, int height) {
 			return;
 		}
 		else {
+			vector<AVPixelFormat> supported_pix_fmt;
 			const AVPixelFormat* p = codec->pix_fmts;
 			while (*p != AV_PIX_FMT_NONE) {
 				ofLogNotice("ofVideoWriter") << "supported pix fmt: " << av_get_pix_fmt_name(*p);
+				supported_pix_fmt.push_back(*p); 
 				++p;
+			}
+			bool pixfmtsupported = false;
+			for (auto formats : supported_pix_fmt) {
+				if (formats == outputFormat)
+					pixfmtsupported = true;
+			}
+			if (p == NULL || *p == AV_PIX_FMT_NONE || !pixfmtsupported) { //if there are no supported pixel formats or the requested format is not one of the supported one
+				if (fmt->video_codec == AV_CODEC_ID_RAWVIDEO) {
+					
+					outputFormat = AV_PIX_FMT_RGB24;
+				}
+				else {
+					outputFormat = AV_PIX_FMT_YUV420P; /* we have to assume the default pix_fmt */
+				}
 			}
 		}
 
@@ -64,7 +81,7 @@ void ofVideoWriter::setup(const char* filename, int width, int height) {
 			return;
 		}
 		video_st->id = oc->nb_streams - 1;
-		video_st->time_base = AVRational{ 1, 25 };
+		video_st->time_base = AVRational{ 1, framerate };
 		c = video_st->codec;
 
 		/* Some formats want stream headers to be separate. */
@@ -76,26 +93,31 @@ void ofVideoWriter::setup(const char* filename, int width, int height) {
 	 * video codecs and allocate the necessary encode buffers. */
 	{
 
-		picture = av_frame_alloc();//avcodec_alloc_frame();
-		picture->pts = 0;
-		picture->width = width;
-		picture->height = height;
+		_outputFrameData = av_frame_alloc();
+		_outputFrameData->pts = 0;
+		_outputFrameData->width = width;
+		_outputFrameData->height = height;
 
 		/* put sample parameters */
-		c->bit_rate = 400000;
+		c->codec_id = fmt->video_codec;
+		c->bit_rate = bitrate;
 		/* resolution must be a multiple of two */
 		c->width = width;
 		c->height = height;
 		/* frames per second */
-		c->time_base = AVRational{ 1, 25 };
+		c->time_base = AVRational{ 1, framerate };
 		c->gop_size = 10; /* emit one intra frame every ten frames */
-		c->max_b_frames = 1;
-		c->pix_fmt = AV_PIX_FMT_YUV420P;
+		//setting b frames results in an error with some codecs
+		/*if(c->has_b_frames)
+			c->max_b_frames = -1;*/
+		c->pix_fmt = outputFormat;
 
 		/* open it */
 		AVDictionary* options = NULL;
-		if (avcodec_open2(c, codec, &options) < 0) {
-			ofLogError("ofVideoWriter") << "could not open codec";
+		int ret = avcodec_open2(c, codec, &options);
+
+		if (ret < 0) {
+			ofLogError("ofVideoWriter") << "Could not open codec " << codec->long_name;
 			return;
 		}
 		else {
@@ -103,32 +125,31 @@ void ofVideoWriter::setup(const char* filename, int width, int height) {
 		}
 
 		/* alloc image and output buffer */
-		picture->data[0] = NULL;
-		picture->linesize[0] = -1;
-		picture->format = c->pix_fmt;
+		_outputFrameData->data[0] = NULL;
+		_outputFrameData->linesize[0] = -1;
+		_outputFrameData->format = c->pix_fmt;
 
-		int ret = -1;
-		ret = av_image_alloc(picture->data, picture->linesize, c->width, c->height, (AVPixelFormat)picture->format, 32);
+		ret = av_image_alloc(_outputFrameData->data, _outputFrameData->linesize, c->width, c->height, (AVPixelFormat)_outputFrameData->format, 32);
 		if (ret < 0) {
-			ofLogError("ofVideoWriter") << "Could not allocate raw picture buffer";
+			ofLogError("ofVideoWriter") << "Could not allocate raw _outputFrameData buffer";
 			return;
 		}
 		else {
 			char buf[256];
-			sprintf(buf, "allocated picture of size %d (ptr %x), linesize %d %d %d %d\n", ret, picture->data[0], picture->linesize[0], picture->linesize[1], picture->linesize[2], picture->linesize[3]);
+			sprintf(buf, "allocated _outputFrameData of size %d (ptr %x), linesize %d %d %d %d\n", ret, _outputFrameData->data[0], _outputFrameData->linesize[0], _outputFrameData->linesize[1], _outputFrameData->linesize[2], _outputFrameData->linesize[3]);
 			ofLogNotice("ofVideoWriter") << buf;
 		}
 
-		picture_rgb24 = av_frame_alloc();//avcodec_alloc_frame();
-		picture_rgb24->format = AV_PIX_FMT_RGB24;
+		_inputFrameData = av_frame_alloc();
+		_inputFrameData->format = inputFormat;
 
-		if ((ret = av_image_alloc(picture_rgb24->data, picture_rgb24->linesize, c->width, c->height, (AVPixelFormat)picture_rgb24->format, 24)) < 0) {
+		if ((ret = av_image_alloc(_inputFrameData->data, _inputFrameData->linesize, c->width, c->height, (AVPixelFormat)_inputFrameData->format, 24)) < 0) {
 			ofLogError("ofVideoWriter") << "cannot allocate RGB temp image";
 			return;
 		}
 		else {
 			char buf[256];
-			sprintf(buf, "allocated picture of size %d (ptr %x), linesize %d %d %d %d\n", ret, picture_rgb24->data[0], picture_rgb24->linesize[0], picture_rgb24->linesize[1], picture_rgb24->linesize[2], picture_rgb24->linesize[3]);
+			sprintf(buf, "allocated _outputFrameData of size %d (ptr %x), linesize %d %d %d %d\n", ret, _inputFrameData->data[0], _inputFrameData->linesize[0], _inputFrameData->linesize[1], _inputFrameData->linesize[2], _inputFrameData->linesize[3]);
 			ofLogNotice("ofVideoWriter") << buf;
 		}
 
@@ -156,9 +177,9 @@ void ofVideoWriter::setup(const char* filename, int width, int height) {
 		return;
 	}
 
-	/* get sws context for RGB24 -> YUV420 conversion */
-	sws_ctx = sws_getContext(c->width, c->height, (AVPixelFormat)picture_rgb24->format,
-		c->width, c->height, (AVPixelFormat)picture->format,
+	/* get sws context for pixel format conversion */
+	sws_ctx = sws_getContext(c->width, c->height, (AVPixelFormat)_inputFrameData->format,
+		c->width, c->height, (AVPixelFormat)_outputFrameData->format,
 		SWS_BICUBIC, NULL, NULL, NULL);
 	if (!sws_ctx) {
 		ofLogError("ofVideoWriter") << "Could not initialize the conversion context";
@@ -169,37 +190,84 @@ void ofVideoWriter::setup(const char* filename, int width, int height) {
 	b_recording = false;
 }
 
-/* add a frame to the video file, RGB 24bpp format */
-void ofVideoWriter::addFrame(const uint8_t* pixels) {
+/* add a frame to the video file, default RGB 24bpp format */
+void ofVideoWriter::addFrame(const ofPixels pixels) {
 	if (b_recording) {
 		/* copy the buffer */
-		memcpy(picture_rgb24->data[0], pixels, size);
+		memcpy(_inputFrameData->data[0], &pixels, size);
 
-		/* convert RGB24 to YUV420 */
-		sws_scale(sws_ctx, picture_rgb24->data, picture_rgb24->linesize, 0, c->height, picture->data, picture->linesize);
+		/* convert pixel formats */
+		sws_scale(sws_ctx, _inputFrameData->data, _inputFrameData->linesize, 0, c->height, _outputFrameData->data, _outputFrameData->linesize);
 
-		AVPacket pkt = { 0 };
-		int got_packet;
-		av_init_packet(&pkt);
-		/* encode the image */
-		int ret = avcodec_encode_video2(c, &pkt, picture, &got_packet);
-		if (ret < 0) {
-			char buf[256];
-			av_strerror(ret, buf, sizeof(buf));
-			ofLogError("ofVideoWriter") << "Error encoding video frame: " << buf;
-			return;
-		}
-		/* If size is zero, it means the image was buffered. */
-		if (!ret && got_packet && pkt.size) {
+		int ret = -1;
+		if (oc->oformat->flags & AVFMT_RAWPICTURE) {
+			/* Raw video case - directly store the picture in the packet */
+			AVPacket pkt;
+			av_init_packet(&pkt);
+			pkt.flags |= AV_PKT_FLAG_KEY;
 			pkt.stream_index = video_st->index;
-			/* Write the compressed frame to the media file. */
+			pkt.data = _outputFrameData->data[0];
+			pkt.size = sizeof(AVPicture);
 			ret = av_interleaved_write_frame(oc, &pkt);
 		}
 		else {
-			ret = 0;
+			AVPacket pkt = { 0 };
+			int got_packet;
+			av_init_packet(&pkt);
+			/* encode the image */
+			int ret = avcodec_encode_video2(c, &pkt, _outputFrameData, &got_packet);
+			if (ret < 0) {
+				char buf[256];
+				av_strerror(ret, buf, sizeof(buf));
+				ofLogError("ofVideoWriter") << "Error encoding video frame: " << buf;
+				return;
+			}
+			/* If size is zero, it means the image was buffered. */
+			if (!ret && got_packet && pkt.size) {
+				pkt.stream_index = video_st->index;
+				/* Write the compressed frame to the media file. */
+				ret = av_interleaved_write_frame(oc, &pkt);
+			}
+			else {
+				ret = 0;
+			}
 		}
-		picture->pts += av_rescale_q(1, video_st->codec->time_base, video_st->time_base);
+		_outputFrameData->pts += av_rescale_q(1, video_st->codec->time_base, video_st->time_base);
 		frame_count++;
+	}
+}
+/* add a frame to the video file, RGB 24bpp format */
+void ofVideoWriter::addAudioFrame(const float* samples) {
+	if (b_recording) {
+		AVSampleFormat sample;
+		///* copy the buffer */
+		//memcpy(_inputFrameData->data[0], pixels, size);
+
+		///* convert RGB24 to YUV420 */
+		//sws_scale(sws_ctx, _inputFrameData->data, _inputFrameData->linesize, 0, c->height, _outputFrameData->data, _outputFrameData->linesize);
+
+		//AVPacket pkt = { 0 };
+		//int got_packet;
+		//av_init_packet(&pkt);
+		///* encode the image */
+		//int ret = avcodec_encode_audio2(c, &pkt, _outputFrameData, &got_packet);
+		//if (ret < 0) {
+		//	char buf[256];
+		//	av_strerror(ret, buf, sizeof(buf));
+		//	ofLogError("ofVideoWriter") << "Error encoding video frame: " << buf;
+		//	return;
+		//}
+		///* If size is zero, it means the image was buffered. */
+		//if (!ret && got_packet && pkt.size) {
+		//	pkt.stream_index = video_st->index;
+		//	/* Write the compressed frame to the media file. */
+		//	ret = av_interleaved_write_frame(oc, &pkt);
+		//}
+		//else {
+		//	ret = 0;
+		//}
+		//_outputFrameData->pts += av_rescale_q(1, video_st->codec->time_base, video_st->time_base);
+		//frame_count++;
 	}
 }
 
@@ -213,8 +281,8 @@ void ofVideoWriter::close() {
 	/* Close each codec. */
 
 	avcodec_close(video_st->codec);
-	av_freep(&(picture->data[0]));
-	av_free(picture);
+	av_freep(&(_outputFrameData->data[0]));
+	av_free(_outputFrameData);
 
 	if (!(fmt->flags & AVFMT_NOFILE))
 		/* Close the output file. */
--- libs/openFrameworks/video/ofVideoWriter.h
+++ libs/openFrameworks/video/ofVideoWriter.h
@@ -10,6 +10,8 @@
 #define __ofVideoWriter__
 
 #include <iostream>
+#include "ofPixels.h"
+
 #ifdef __cplusplus
 extern "C" {
 #include <libavcodec/avcodec.h>
@@ -20,28 +22,33 @@ extern "C" {
 class ofVideoWriter {
 
 public:
-    ofVideoWriter():oc(NULL),codec(NULL),initialized(false),frame_count(1),b_recording(false) {}
-    
-    /**
-     * setup the video writer
-     * @param output filename, the codec and format will be determined by it. (e.g. "xxx.mpg" will create an MPEG1 file
-     * @param width of the frame
-     * @param height of the frame
-     **/
-    void setup(const char* filename, int width, int height);
-    /**
-     * add a frame to the video file
-     * @param the pixels packed in RGB (24-bit RGBRGBRGB...)
-     **/
-    void addFrame(const uint8_t* pixels);
-    /**
-     * close the video file and release all datastructs
-     **/
-    void close();
-    /**
-     * is the videowriter initialized?
-     **/
-    bool isInitialized() const { return initialized; }
+	ofVideoWriter() :oc(NULL), codec(NULL), initialized(false), frame_count(1), b_recording(false) {}
+
+	/**
+	 * setup the video writer
+	 * @param output filename, the codec and format will be determined by it. (e.g. "xxx.mpg" will create an MPEG1 file
+	 * @param width of the frame
+	 * @param height of the frame
+	 **/
+	void setup(const char* filename, int width, int height, int bitrate = 400000, int framerate = 25, AVPixelFormat inputFormat = AV_PIX_FMT_RGB24, AVPixelFormat outputFormat = AV_PIX_FMT_YUV420P);
+	/**
+	 * add a frame to the video file
+	 * @param the pixels packed in RGB (24-bit RGBRGBRGB...)
+	 **/
+	void addFrame(const ofPixels pixels);
+	/**
+	* add a frame to the video file
+	* @param the pixels packed in RGB (24-bit RGBRGBRGB...)
+	**/
+	void addAudioFrame(const float* samples);
+	/**
+	 * close the video file and release all datastructs
+	 **/
+	void close();
+	/**
+	 * is the videowriter initialized?
+	 **/
+	bool isInitialized() const { return initialized; }
 	bool isRecording() const { return b_recording; }
 	void start() { if (initialized) b_recording = true; }
 
@@ -49,7 +56,7 @@ private:
 	//instance variables
 	AVCodec *codec;
 	int size, frame_count;
-	AVFrame *picture, *picture_rgb24;
+	AVFrame *_outputFrameData, *_inputFrameData;
 	struct SwsContext *sws_ctx;
 	AVOutputFormat *fmt;
 	AVFormatContext *oc;
